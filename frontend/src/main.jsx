6c020495137726d2157a38aa766d14da68d57391
SJammy <samplay@one.com>
2023-06-27 22:51:28 +0100

    Add product display

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
+
 import App from './App.jsx'
-import './index.css'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route  path="/product/:id" element={<ProductScreen />} />
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
